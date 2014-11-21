2cf5187b194f31960063884a07d66c68813f2110
Shawna <shawna@appacademy.io>
2014-11-21 13:58:08 -0800

    pattern model

--- spec/models/pattern_spec.rb
+++ spec/models/pattern_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Pattern, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject(:pattern) { Pattern.new(name: "Totoro") }
+
+  it "is invalid without a name" do
+    expect(pattern).to be_valid
+
+    pattern.name = nil
+    expect(pattern).to_not be_valid
+  end
+
+  it "cannot have a repeat name" do
+    repeat_pattern = pattern.dup
+    repeat_pattern.save
+    expect(pattern).to_not be_valid
+  end
 end
