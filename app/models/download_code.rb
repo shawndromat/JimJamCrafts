488b284175f62fe1d0c37c02b421cfd29a679acf
Shawna <shawna@appacademy.io>
2015-01-17 15:10:06 -0800

    easier to read codes

--- app/models/download_code.rb
+++ app/models/download_code.rb
@@ -9,10 +9,20 @@ class DownloadCode < ActiveRecord::Base
 
   default_scope { order(:created_at) }
 
+  CHARS_TO_SUB = {
+    /I/ => "i",
+    /l/ => "L",
+    /O/ => "0"
+  }
+
   def self.unique_code
     code = nil
     loop do
       code = SecureRandom::urlsafe_base64(8)
+      p code
+      CHARS_TO_SUB.each do |regex, char|
+        code = code.gsub(regex, char)
+      end
       break unless DownloadCode.exists?(code: code)
     end
 
